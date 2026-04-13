d8434a0b3bf39877adfd74bddf70b06ec2d51728
Rohit Kumawat <rkumawat125@gmail.com>
2026-04-13 08:12:02 +0530

    Add: longest-consecutive-sequence - submission-11

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.js
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const obj = {};
+        const out = {};
+        let i = 0;
+
+        nums.forEach((num, idx) => {
+            if (!obj[num]) {
+                obj[num] = 1;
+            }
+        })
+
+        nums.forEach((num, idx) => {
+            if (!obj[num-1]) {
+                let j = num+1;
+                let start = num;
+                out[start] = [];
+
+                while (obj[j]) {
+                    out[start].push(j);
+                    j = j+1;
+                }
+            }
+        })
+
+        let largestKey;
+        let largest = 0;
+        for (let [key, value] of Object.entries(out)) {
+            if (value.length > largest) {
+                largestKey = key;
+                largest = value.length;
+            }
+        }
+
+        let output = 0;
+
+        if (largestKey === undefined) {
+            console.log("out", out);
+            largestKey = Object.keys(out)[0];
+        }
+
+        console.log("largestKey", largestKey, out)
+        if (largestKey !== undefined) {
+            output = out[largestKey].push(largestKey);
+        }
+
+        return output;
+
+    }
+}
