cee602b7634e2c834b19194f21f1e11a8dd26e26
Rohit Kumawat <rkumawat125@gmail.com>
2026-04-23 19:48:41 +0530

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.js
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * **/
+// class ListNode {
+//     constructor(val = 0, next = null) {
+//         this.val = val;
+//         this.next = next;
+//     }
+// }
+ 
+
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1, list2) {
+        let curr1 = list1;
+        let curr2 = list2;
+        let curr = null;
+        let resultHead = null;
+
+        while (curr1 !== null && curr2 !== null) {
+            const min = curr1.val < curr2.val ? curr1.val : curr2.val;
+            
+            console.log("curr", curr)
+            
+            if (curr === null) {
+                curr = new ListNode();
+                curr.val = min;
+                resultHead = curr;
+            } else {
+                curr.next = new ListNode();
+                curr.next.val = min;
+                curr = curr.next;
+            }
+
+            if (curr1.val < curr2.val) {
+                curr1 = curr1.next;
+            } else {
+                curr2 = curr2.next;
+            }
+        }
+
+        while (curr1 !== null) {
+            if (curr === null) {
+                curr = new ListNode();
+                curr.val = curr1.val;
+                resultHead = curr;
+            } else {
+                curr.next = new ListNode();
+                curr.next.val = curr1.val;
+                curr = curr.next;
+            }
+            
+            curr1 = curr1.next;
+        }
+
+        while (curr2 !== null) {
+            if (curr === null) {
+                curr = new ListNode();
+                curr.val = curr2.val;
+                resultHead = curr;
+            } else {
+                curr.next = new ListNode();
+                curr.next.val = curr2.val;
+                curr = curr.next;
+            }
+
+            curr2 = curr2.next;
+        }
+
+        return resultHead;
+    }
+}
