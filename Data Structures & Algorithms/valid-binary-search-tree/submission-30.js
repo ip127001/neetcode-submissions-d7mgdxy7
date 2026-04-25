0e6ec502fd15b7d29a9be803066de774357baa17
Rohit Kumawat <rkumawat125@gmail.com>
2026-04-25 16:01:10 +0530

    Add: valid-binary-search-tree - submission-30

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-30.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+
+    isValid(root, prev) {
+        if (root === null) {
+            return true;
+        }
+
+        if (!this.isValid(root.left, prev)) {
+            return false;
+        }
+
+        if (root.val < prev) {
+            return false;
+        } else if (root.val > prev) {
+            prev.pop();
+            prev.push(root.val);
+        } else {
+            return false;
+        }
+
+        return this.isValid(root.right, prev)
+    }
+
+    isValidBST(root) {
+        const prev = [-Infinity];
+        return this.isValid(root, prev)
+    }
+}
