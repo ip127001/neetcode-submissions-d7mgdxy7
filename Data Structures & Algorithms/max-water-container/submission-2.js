382dd50119e433092384b34bec186694e75a36ad
Rohit Kumawat <rkumawat125@gmail.com>
2026-06-15 10:45:48 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let left = 0;
+        let right = heights.length-1;
+        let maxOutput = 0;
+
+        while (left <= right) {
+            const width = right - left;
+            const output = width * Math.min(heights[left], heights[right]);
+
+            maxOutput = Math.max(maxOutput, output);
+
+            if (heights[left] <= heights[right]) {
+                left++
+            } else {
+                right--;
+            }
+        }
+        
+        return maxOutput;
+    }
+}
